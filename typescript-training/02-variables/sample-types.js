00c5ad6a703fd5f070956ad2db3ac4faf8d70514
Nitin Kanwar <kanwarnitin@gmail.com>
2022-11-08 23:07:50 +0000

    added

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,6 @@
+var found = true;
+var grade = 88.6;
+var firstName = "John";
+var lastName = "Doe";
+console.log("The grade is " + grade);
+console.log("My name is ".concat(firstName, " ").concat(lastName));
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,7 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "John";
+let lastName: string = "Doe";
+
+console.log("The grade is " + grade);
+console.log(`My name is ${firstName} ${lastName}`)
